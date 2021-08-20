11183ed253df4298af6bc4fc8d38ab46ec724fb7
Andrey Voronin <AVor0n@yandex.ru>
2021-08-20 17:23:23 +0500

    add: possibility to choose a flat

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
 $(document).ready(function () {
     let currentFloor = 2;
-    let counterUp = $('.counter-up')
-    let counterDown = $('.counter-down')
-    $('.home-image path').on('mouseover', function () {
-        $('.home-image path').removeClass('current-floor');
+    const counterUp = $('.counter-up')
+    const counterDown = $('.counter-down')
+    const floorPath = $('.home-image path')
+    const modal = $('.modal')
+    const modalCloseButton = $('.modal-close-button')
+    const modalOpenButton = $('.button-primary')
+    const toggleModal = () => modal.toggleClass('is-open')
+
+    floorPath.on('mouseover', function () {
+        floorPath.removeClass('current-floor');
         $(this).toggleClass('current-floor');
         currentFloor = $(this).attr('data-floor');
         $('.counter').text(currentFloor);
     })
+
+    floorPath.on('click', toggleModal)
+    modalOpenButton.on('click', toggleModal)
+    modalCloseButton.on('click', toggleModal)
 
     counterUp.on('click', function () {
         if (currentFloor < 18) {
             currentFloor++;
             usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
             $('.counter').text(usCurrentFloor);
-            $('.home-image path').removeClass('current-floor');
+            floorPath.removeClass('current-floor');
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
     })
@@ -24,8 +34,24 @@ $(document).ready(function () {
             currentFloor--;
             usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
             $('.counter').text(usCurrentFloor);
-            $('.home-image path').removeClass('current-floor');
+            floorPath.removeClass('current-floor');
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
+    })
+
+    const flatPath = $('.floor-image path')
+    const flatLink = $('.flat-link')
+
+    flatLink.on('mouseover', function () {
+        let idxFlat = $(this).attr('data-flat');
+        flatPath.removeClass('current-flat')
+        flatLink.removeClass('current-flat')
+        $(`[data-flat=${idxFlat}]`).toggleClass('current-flat')
+    })
+    flatPath.on('mouseover', function () {
+        let idxFlat = $(this).attr('data-flat');
+        flatPath.removeClass('current-flat')
+        flatLink.removeClass('current-flat')
+        $(`[data-flat=${idxFlat}]`).toggleClass('current-flat')
     })
 });
