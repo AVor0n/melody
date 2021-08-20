d98be6e0bbbfffce291adba77eeafdb6ab6448ff
Andrey Voronin <AVor0n@yandex.ru>
2021-08-20 19:22:02 +0500

    refactor: add comments

--- js/main.js
+++ js/main.js
@@ -1,57 +1,63 @@
 $(document).ready(function () {
+
+    //---------------------Обработка действий по выбору этажа
     let currentFloor = 2;
     const counterUp = $('.counter-up')
     const counterDown = $('.counter-down')
-    const floorPath = $('.home-image path')
-    const modal = $('.modal')
-    const modalCloseButton = $('.modal-close-button')
-    const modalOpenButton = $('.button-primary')
-    const toggleModal = () => modal.toggleClass('is-open')
+    const floorPath = $('.home-image path') //svg-контуры для подсветки выбранного этажа
 
+    function setCurrentFloor(floor) {
+        currentFloor = floor
+
+        usCurrentFloor = String(floor).padStart(2, '0')
+        $('.counter').text(usCurrentFloor)
+
+        floorPath.removeClass('current-floor')
+        $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor')
+    }
+
+    //Изменение счетчика при наведении на картику этажа
     floorPath.on('mouseover', function () {
-        floorPath.removeClass('current-floor');
-        $(this).toggleClass('current-floor');
-        currentFloor = $(this).attr('data-floor');
-        $('.counter').text(currentFloor);
+        setCurrentFloor($(this).attr('data-floor'))
     })
-
-    floorPath.on('click', toggleModal)
-    modalOpenButton.on('click', toggleModal)
-    modalCloseButton.on('click', toggleModal)
-
     counterUp.on('click', function () {
         if (currentFloor < 18) {
-            currentFloor++;
-            usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-            $('.counter').text(usCurrentFloor);
-            floorPath.removeClass('current-floor');
-            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+            setCurrentFloor(++currentFloor)
         }
     })
-
     counterDown.on('click', function () {
         if (currentFloor > 2) {
-            currentFloor--;
-            usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-            $('.counter').text(usCurrentFloor);
-            floorPath.removeClass('current-floor');
-            $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+            setCurrentFloor(--currentFloor)
         }
     })
 
-    const flatPath = $('.floor-image path')
-    const flatLink = $('.flat-link')
 
-    flatLink.on('mouseover', function () {
-        let idxFlat = $(this).attr('data-flat');
+    //-------------------Обработка действий с модальным окном по выбору квартиры
+    const modal = $('.modal') //модальное окно
+    const modalCloseButton = $('.modal-close-button')
+    const modalOpenButton = $('.button-primary')
+    const flatPath = $('.floor-image path') //svg-контуры для подсветки доступных квартир
+    const flatLink = $('.flat-link') //список с ссылками доступных квартир
+
+    function toggleModal() {
+        modal.toggleClass('is-open')
+    }
+
+    function selectCurrentFlat() {
+        let currentFlat = $(this).attr('data-flat');
         flatPath.removeClass('current-flat')
         flatLink.removeClass('current-flat')
-        $(`[data-flat=${idxFlat}]`).toggleClass('current-flat')
-    })
-    flatPath.on('mouseover', function () {
-        let idxFlat = $(this).attr('data-flat');
-        flatPath.removeClass('current-flat')
-        flatLink.removeClass('current-flat')
-        $(`[data-flat=${idxFlat}]`).toggleClass('current-flat')
-    })
+        $(`[data-flat=${currentFlat}]`).toggleClass('current-flat')
+    }
+
+    //Открытие модального окна с выбором квартиры
+    floorPath.on('click', toggleModal) //при клике на картинку этажа
+    modalOpenButton.on('click', toggleModal) //при нажатии на кнопку "Посмотреть квартиры"
+
+    //Закрытие модального окна
+    modalCloseButton.on('click', toggleModal)
+
+    //Подсвечивание выбранной квартиры
+    flatLink.on('mouseover', selectCurrentFlat) //при выборе в списке
+    flatPath.on('mouseover', selectCurrentFlat) //при выборе на картинке
 });
